e674fdad4f195029c5255a0b809372022fefadcc
Josh <josh_2021@yahoo.com>
2021-03-16 15:26:09 -0400

    added controller

--- CommunityLibrary/Controllers/HomeController.cs
+++ CommunityLibrary/Controllers/HomeController.cs
@@ -176,41 +176,15 @@ namespace CommunityLibrary.Controllers
 
         public IActionResult UserMap()
         {
-
             // Grab users lat and lng
             string user = User.FindFirst(ClaimTypes.NameIdentifier).Value;
             User currentUser = _libraryDB.Users.First(x => x.UserId == user);
             TempData["lat"] = currentUser.Latitude;
             TempData["lng"] = currentUser.Longitude;
 
-            // grab  everyone else's lat and lng from table
-            // create a marker list of lat and lngs close to user
-            List<User> notLogged = _libraryDB.Users.Where(x => x.UserId != user).ToList();
-            List<User> withinDistance = new List<User>();
-            int max = 0; // only show so many libraries
-
-            const double radConv = Math.PI / 180;
-            const int R = 6371; // radius of earth in km
-            const int d = 20; // only show libraries within 20km
-            double userLat = Double.Parse(currentUser.Latitude) * radConv;
-            double otherLat;
-            double deltaLng;
-            foreach (User other in notLogged)
-            {
-                otherLat = Double.Parse(other.Latitude) * radConv;
-                deltaLng = Double.Parse(currentUser.Longitude) - Double.Parse(other.Longitude);
-
-                if ((Math.Acos(Math.Sin(userLat) * Math.Sin(otherLat) + Math.Cos(userLat) * Math.Cos(otherLat) * Math.Cos(deltaLng)) * R) < d)
-                {
-                    withinDistance.Add(other);
-                }
-
-                if (max >= 15)
-                {
-                    break;
-                }
-                max++;
-            }
+            // get local users
+            List<User> notUser = _libraryDB.Users.Where(x => x.UserId != user).ToList();
+            List<User> withinDistance = GetLocalUsers(currentUser,notUser);
 
             return View(withinDistance);
         }
@@ -218,14 +192,16 @@ namespace CommunityLibrary.Controllers
         {
             BookInfo apiBook = _libraryDAL.GetBookInfo(bookId);
             List<Author> authors = new List<Author>();
-            foreach (Author author in apiBook.authors)
-            {
-                string authorId = author.author.key;
-                Author apiAuthor = _libraryDAL.GetAuthorInfo(authorId);
+          
+              foreach (Author author in apiBook.authors)
+                {
+                    string authorId = author.author.key;
+                    Author apiAuthor = _libraryDAL.GetAuthorInfo(authorId);
 
-                authors.Add(apiAuthor);
-            }
-            apiBook.authors = authors;
+                    authors.Add(apiAuthor);
+                }
+                apiBook.authors = authors;
+            
             return View(apiBook);
         }
 
@@ -255,7 +231,6 @@ namespace CommunityLibrary.Controllers
 
         }
 
-        [Authorize]
         public IActionResult ReviewBook(string bookId)
         {
             string user = User.FindFirst(ClaimTypes.NameIdentifier).Value;
@@ -275,7 +250,6 @@ namespace CommunityLibrary.Controllers
             }
         }
 
-        [Authorize]
         [HttpPost]
         public IActionResult ReviewBook(BookReview bookReview)
         {
@@ -293,7 +267,6 @@ namespace CommunityLibrary.Controllers
             return View();
         }
 
-        [Authorize]
         public IActionResult MyBookReviews()
         {
             string user = User.FindFirst(ClaimTypes.NameIdentifier).Value;
@@ -319,7 +292,18 @@ namespace CommunityLibrary.Controllers
 
             return View(results);
         }
-        
+
+        public IActionResult ViewLocalLibraries()
+        {
+            string user = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            User currentUser = _libraryDB.Users.First(x => x.UserId == user);
+
+            // get local users
+            List<User> notUser = _libraryDB.Users.Where(x => x.UserId != user).ToList();
+            List<User> withinDistance = GetLocalUsers(currentUser, notUser);
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
@@ -329,6 +313,39 @@ namespace CommunityLibrary.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public static List<User> GetLocalUsers(User currentUser, List<User> notUser)
+        {
+            List<User> within = new List<User>();
+            // grab  everyone else's lat and lng from table
+            // create a marker list of lat and lngs close to user
+    
+            int max = 0; // only show so many libraries
+
+            const double radConv = Math.PI / 180;
+            const int R = 6371; // radius of earth in km
+            const int d = 20; // only show libraries within 20km
+            double userLat = Double.Parse(currentUser.Latitude) * radConv;
+            double otherLat;
+            double deltaLng;
+            foreach (User other in notUser)
+            {
+                otherLat = Double.Parse(other.Latitude) * radConv;
+                deltaLng = Double.Parse(currentUser.Longitude) - Double.Parse(other.Longitude);
+
+                if ((Math.Acos(Math.Sin(userLat) * Math.Sin(otherLat) + Math.Cos(userLat) * Math.Cos(otherLat) * Math.Cos(deltaLng)) * R) < d)
+                {
+                    within.Add(other);
+                }
+
+                if (max >= 15)
+                {
+                    break;
+                }
+                max++;
+            }
+            return within;
         }
     }
 }
